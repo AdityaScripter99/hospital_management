a576d5da373beccc0ec3788c9c849c4619085d8c
AdityaScripter99~ <arpitaaditya1234@gmail.com>
2024-12-20 20:28:09 +0530

    Update

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from appointments.views import book_appointment,doctor_dashboard,patient_dashboard,update_appointment_status
 
 urlpatterns = [
-    path('book/', views.book_appointment, name='book_appointment'),
-    path('doctor/dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
-    path('appointment/<int:appointment_id>/status/', views.update_appointment_status, name='update_appointment_status'),
-    path('patient/dashboard/', views.patient_dashboard, name='patient_dashboard'),
+    path('book/',book_appointment, name='book_appointment'),
+    path('doctor/dashboard/',doctor_dashboard, name='doctor_dashboard'),
+    path('appointment/<int:appointment_id>/status/',update_appointment_status, name='update_appointment_status'),
+    path('patient/dashboard/',patient_dashboard, name='patient_dashboard'),
 ]
